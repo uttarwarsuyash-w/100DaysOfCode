cd1ea296827a0bfdbbb95886cb3254885c826afe
uttarwarsuyash-w <uttarwarsuyash@gmail.com>
2026-06-26 17:00:40 +0530

    Day 1 completed

---
+++ Day01/average.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b,c;
+    cout<<"enter three numbers:";
+    cin>>a>>b>>c;
+    cout<<"average of three numbers is="<<(a+b+c)/3<<endl;
+    return 0;
+}
---
+++ Day01/product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter two numbers:";
+    cin>>a>>b;
+    cout<<"product of two numbers is="<<a*b<<endl;
+    return 0;
+
+}
